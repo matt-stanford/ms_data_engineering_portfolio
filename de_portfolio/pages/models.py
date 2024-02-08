1e5ec92cc1aa9ddb64a421300d1535d3f1d99f80
matt-stanford <mjfstanford@gmail.com>
2024-02-08 21:59:27 +0000

    added portfolio slider

---
+++ de_portfolio/pages/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    summary = models.TextField()
+    technology = models.CharField(max_length=255)
+    url = models.URLField(blank=True)
+    github_url = models.URLField(blank=True)
+    image_main = models.ImageField(upload_to='images/')
+    image_1 = models.ImageField(upload_to='images/', blank=True)
+    image_2 = models.ImageField(upload_to='images/', blank=True)
+    image_3 = models.ImageField(upload_to='images/', blank=True)
+    image_4 = models.ImageField(upload_to='images/', blank=True)
+    image_5 = models.ImageField(upload_to='images/', blank=True)
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def short_summary(self):
+        return f'{self.summary[:100]}...'
+
+    def __str__(self):
+        return self.title
+
---
+++ de_portfolio/theme/static_src/tailwind.config.js
@@ -0,0 +1,85 @@
+/**
+ * This is a minimal config.
+ *
+ * If you need the full config, get it from here:
+ * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
+ */
+
+module.exports = {
+    content: [
+        /**
+         * HTML. Paths to Django template files that will contain Tailwind CSS classes.
+         */
+
+        /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
+        '../templates/**/*.html',
+
+        /*
+         * Main templates directory of the project (BASE_DIR/templates).
+         * Adjust the following line to match your project structure.
+         */
+        '../../templates/**/*.html',
+
+        /*
+         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
+         * Adjust the following line to match your project structure.
+         */
+        '../../**/templates/**/*.html',
+
+        /**
+         * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
+         * patterns match your project structure.
+         */
+        /* JS 1: Ignore any JavaScript in node_modules folder. */
+        // '!../../**/node_modules',
+        /* JS 2: Process all JavaScript files in the project. */
+        // '../../**/*.js',
+
+        /**
+         * Python: If you use Tailwind CSS classes in Python, uncomment the following line
+         * and make sure the pattern below matches your project structure.
+         */
+        // '../../**/*.py'
+    ],
+    theme: {
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+        },
+        extend: {
+            colors: {
+                'primary-blue-100': '#e4f7f7',
+                'primary-blue-200': '#a4e0fa',
+                'primary-blue-300': '#63c9fc',
+                'primary-blue-400': '#46b3e8',
+                'primary-blue-500': '#289cd4',
+                'primary-blue-600': '#177bac',
+                'primary-blue-700': '#065a84',
+                'primary-blue-800': '#054262',
+                'primary-blue-900': '#032a40',
+            }
+        },
+    },
+    plugins: [
+        /**
+         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+         * for forms. If you don't like it or have own styling for forms,
+         * comment the line below to disable '@tailwindcss/forms'.
+         */
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
+}
